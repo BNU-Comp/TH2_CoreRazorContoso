4abcacdfb84af4666bddbca87ba93dcdeaad05ba
Derek Peacock <derek_peacock@yahoo.com>
2019-10-10 11:41:28 +0100

    Add project files.

---
+++ TH2_CoreRazerContoso/Pages/Converter.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TH2_CoreRazerContoso.Pages
+{
+    public class ConverterModel : PageModel
+    {
+        public Decimal Pounds { get; set; }
+        public Decimal Euros { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+
+        }
+    }
+}
