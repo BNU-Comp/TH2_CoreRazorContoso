79b7d9dab47501de6783d7715f271a444a331bfa
DerekPeacock <derek.peacock@yahoo.com>
2019-10-15 11:30:46 +0100

    Updated the currency converter

--- TH2_CoreRazerContoso/Pages/Converter.cshtml.cs
+++ TH2_CoreRazerContoso/Pages/Converter.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -10,15 +11,26 @@ namespace TH2_CoreRazerContoso.Pages
     public class ConverterModel : PageModel
     {
         public Decimal Pounds { get; set; }
+
         public Decimal Euros { get; set; }
+
+        public CultureInfo Portugal = new CultureInfo("pt-PT");
+
+        public string currencyFormat = "C2";
 
         public void OnGet()
         {
-
+            Pounds = 0m;
+            Euros = 0m;
         }
+
+
         public void OnPost()
         {
+            string value = Request.Form["Pounds"];
+            Pounds = Convert.ToDecimal(value);
 
+            Euros = Pounds * 1.12m;
         }
     }
 }
