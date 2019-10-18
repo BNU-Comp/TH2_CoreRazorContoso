1771a02567116b04eb352854d06611d867f19557
DerekPeacock <derek.peacock@yahoo.com>
2019-10-18 14:22:35 +0100

    Added three model classes

---
+++ TH2_CoreRazerContoso/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TH2_CoreRazerContoso.Models
+{
+    public class Student
+    {
+        // Primary Key
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        // Navigation Property
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
