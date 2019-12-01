054f0b02f44f3a3d13952126716576511aae2f24
DerekPeacock <derek.peacock@yahoo.com>
2019-12-01 14:19:07 +0000

    Completed adding a Courses public page with authorisation

---
+++ TH2_CoreRazerContoso/Pages/Enrol.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TH2_CoreRazerContoso.Models;
+
+namespace TH2_CoreRazerContoso.Pages
+{
+    [Authorize]
+    public class EnrolModel : PageModel
+    {
+        private readonly UniversityContext _context;
+
+        public EnrolModel(UniversityContext context)
+        {
+            _context = context;
+        }
+
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Course = await _context.Course.FirstOrDefaultAsync(m => m.CourseID == id);
+
+            if (Course == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
